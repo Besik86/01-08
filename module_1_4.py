1dd40b84517664ef497d4bfa0920eb66703ad651
Besik86 <besik86@gmail.com>
2024-08-11 12:28:07 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -2,5 +2,6 @@ my_string = input('Организация программ и методы ст�
 print(my_string .upper())
 print(my_string .lower())
 print(my_string .replace(' ',''))
+print(my_string [0])
 print(my_string [-1])
 print(len(my_string))
